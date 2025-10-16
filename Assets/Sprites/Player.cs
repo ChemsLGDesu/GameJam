901f1de9c2cf4f742010ead04013fffb79be8727
diegoapena <diego.apena@tecsup.edu.pe>
2025-10-16 11:27:25 -0500

    Anvance1
    mov
    camara

---
+++ Assets/Sprites/Player.cs
@@ -0,0 +1,44 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public InputSystem_Actions input;
+    public float speed;
+    public Vector2 moveInput;
+    private void Awake()
+    {
+        input = new();
+    }
+
+    private void OnEnable()
+    {
+        input.Enable();
+
+        input.Player.Move.started += OnMove;
+        input.Player.Move.performed += OnMove;
+        input.Player.Move.canceled += OnMove;
+    }
+    private void OnDisable()
+    {
+        input.Disable();
+
+        input.Player.Move.started -= OnMove;
+        input.Player.Move.performed -= OnMove;
+        input.Player.Move.canceled -= OnMove;
+    }
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+    private void Update()
+    {
+        MovementMechanics();
+    }
+    public void MovementMechanics()
+    {
+        transform.position += (Vector3)moveInput * speed * Time.deltaTime;
+    }
+}
