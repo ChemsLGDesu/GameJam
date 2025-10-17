9e504f4f5dbfc2405775750de508c7c6cc02675e
wassup-999 <sebasquispeperez@gmail.com>
2025-10-17 14:46:29 -0500

    Se comiteo algo

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private int counter = 0;
+    [SerializeField] private Player player;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
